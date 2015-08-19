4edb3de205de027f38a8ac9e89ee30b013de862a
dnmsk <dmitry.klimanov@gmail.com>
2015-08-19 22:37:32 +0300

    fix build step

--- Code/UnitTestProject/TestServiceEnviroment.cs
+++ Code/UnitTestProject/TestServiceEnviroment.cs
@@ -1,4 +1,6 @@
-﻿using CommonUtils.Core.Config;
+﻿using System;
+using System.Xml;
+using CommonUtils.Core.Config;
 using IDEV.Hydra.DAO;
 using IDEV.Hydra.DAO.DbConfig;
 using NUnit.Framework;
@@ -9,8 +11,23 @@ namespace UnitTestProject {
         [SetUp]
         public void SetUp() {
             ConfigHelper.TestMode = true;
-            var target = ConfigHelper.LocalConfigPath;
-            DatabaseActions.AddConnectionOption("Master", new ConnectOptions("Server=localhost;Port=5432;Database=rproject_db;User ID=postgres;Password=Ingate2009;", 10, 2, DbServerType.PostgreSql));
+            ConfigHelper.RegisterConfigTarget("Application.xml", xml => {
+                var xmlDoc = (XmlDocument) xml;
+                var container = xmlDoc.GetElementsByTagName("ConnectionStrings");
+                if (container.Count != 1) {
+                    return;
+                }
+                foreach (XmlNode connectionItem in container[0].ChildNodes) {
+                    DatabaseActions.AddConnectionOption(
+                        connectionItem.Attributes["name"].Value,
+                        new ConnectOptions(
+                            connectionItem.Attributes["connectionString"].Value,
+                            uint.Parse(connectionItem.Attributes["maxConnCount"].Value),
+                            uint.Parse(connectionItem.Attributes["maxRetries"].Value),
+                            (DbServerType) Enum.Parse(typeof(DbServerType), connectionItem.Attributes["dbServerType"].Value))
+                    );
+                }
+            });
         }
     }
 }
