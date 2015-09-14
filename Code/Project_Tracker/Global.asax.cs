f2d8c212d3a191a1d0a131aa8df70b7a3126b3a6
dnmsk <dmitry.klimanov@gmail.com>
2015-09-14 13:52:41 +0300

    Fix default configuration for R

--- Code/Project_Tracker/Global.asax.cs
+++ Code/Project_Tracker/Global.asax.cs
@@ -1,10 +1,14 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Routing;
 
-namespace Project_Tracker {
-    public class MvcApplication : HttpApplication {
-        protected void Application_Start() {
+namespace Project_Tracker
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
             AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
