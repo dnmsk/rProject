f3c8092faef1a027ab5ffeb5da91d6d8b51ed5e5
Dmitry Klimanov <dmitry.klimanov@gmail.com>
2015-10-12 13:59:31 +0300

    Project_B. Add task to start

--- Code/Project_B/Global.asax.cs
+++ Code/Project_B/Global.asax.cs
@@ -1,11 +1,19 @@
 ﻿using System.Web.Routing;
 using MainLogic.WebFiles;
+using Project_B.Code.Algorythm;
 
 namespace Project_B {
     public class MvcApplication : GlobalAsaxBase {
+        private static object[] _taskObjects = null;
 
         public override void OnStart() {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            if (SiteConfiguration.NeedRunTask) {
+                _taskObjects = new[] {
+                    new CollectHistoryAlgo(),
+                };
+            }
         }
     }
 }
