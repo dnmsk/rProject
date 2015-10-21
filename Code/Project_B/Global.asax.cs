ba6ccfb4c5a574451d4b12478c3898077eb5deef
Dmitry Klimanov <dmitry.klimanov@gmail.com>
2015-10-22 01:11:55 +0300

    Project_B. Return task objects.

--- Code/Project_B/Global.asax.cs
+++ Code/Project_B/Global.asax.cs
@@ -1,14 +1,23 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
 using MainLogic.WebFiles;
+using Project_B.Code.Algorythm;
 
 namespace Project_B {
     public class MvcApplication : GlobalAsaxBase {
+        private static object[] _taskObjects;
 
         public override void OnStart() {
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles();
+            if (SiteConfiguration.NeedRunTask && _taskObjects == null) {
+                _taskObjects = new object[] {
+                    new CollectHistoryAlgo(),
+                    new CollectOddsAlgo(),
+                    new CollectLiveOddsWithResultAlgo()
+                };
+            }
         }
     }
 }
