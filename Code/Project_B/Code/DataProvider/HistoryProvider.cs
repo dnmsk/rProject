68357208bda150e65d59f0a2c06efdfc51a283d1
Dmitry Klimanov <dmitry.klimanov@gmail.com>
2015-10-30 23:05:25 +0300

    Project_B. Statistics for Saving

--- Code/Project_B/Code/DataProvider/HistoryProvider.cs
+++ Code/Project_B/Code/DataProvider/HistoryProvider.cs
@@ -27,18 +27,30 @@ namespace Project_B.Code.DataProvider {
                         algoMode = algoMode.FlagSet(GatherBehaviorMode.CreateIfNew);
                         _logger.Info("Date {0} enable GatherBehaviorMode.CreateIfNew", minDate);
                     }
-                }       
+                }
+                var successCompetitions = 0;
+                var successCompetitionItems = 0;
                 foreach (var competitionParsed in brokerData.Competitions) {
                     var competition = MainProvider.Instance.CompetitionProvider.GetCompetition(brokerData.Language, competitionParsed.Type, competitionParsed.Name, competitionParsed, algoMode);
+                    if (competition == null) {
+                        continue;
+                    }
+                    successCompetitions++;
                     foreach (var matchParsed in competitionParsed.Matches) {
                         var competitor1 = MainProvider.Instance.CompetitorProvider
                             .GetCompetitor(brokerData.Language, competitionParsed.Type, competition.GenderType, matchParsed.CompetitorNameFullOne, matchParsed.CompetitorNameShortOne, competition.UniqueID, matchParsed, algoMode);
                         var competitor2 = MainProvider.Instance.CompetitorProvider
                             .GetCompetitor(brokerData.Language, competitionParsed.Type, competition.GenderType, matchParsed.CompetitorNameFullTwo, matchParsed.CompetitorNameShortTwo, competition.UniqueID, matchParsed, algoMode);
+                        if (competitor1 == null || competitor2 == null) {
+                            continue;
+                        }
+                        successCompetitionItems++;
                         var competitionItem = MainProvider.Instance.CompetitionProvider.GetCompetitionItem(competitor1, competitor2, competition, matchParsed.DateUtc, algoMode);
                         MainProvider.Instance.ResultProvider.SaveResults(competitionItem, competitionParsed.Type, matchParsed.Result);
                     }
                 }
+                _logger.Info("SaveResults: {0}: {1}/{2} {3}/{4} {5} {6}", brokerData.Competitions.First().Matches.First().DateUtc.Date.ToString("yyyy MMMM dd"), successCompetitions, brokerData.Competitions.Count, 
+                    successCompetitionItems, brokerData.Competitions.Sum(c => c.Matches.Count), brokerData.Broker, brokerData.Language);
             });
         }
 
