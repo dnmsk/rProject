47305566b00e012f4de04da0ff369711ee0aba5d
Dmitry Klimanov <dmitry.klimanov@gmail.com>
2015-12-27 20:37:20 +0300

    Project_B. Losted files.

---
+++ Code/Project_B/CodeServerSide/DataProvider/DataHelper/RawData/RawObject.cs
@@ -0,0 +1,6 @@
+﻿namespace Project_B.CodeServerSide.DataProvider.DataHelper.RawData {
+    public class RawObject {
+        public int ID { get; set; }
+        public int ParentID { get; set; }
+    }
+}
---
+++ Code/Project_B/CodeServerSide/DataProvider/DataHelper/RawData/RawTemplateObj.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_B.CodeServerSide.DataProvider.DataHelper.RawData {
+    public class RawTemplateObj<T> where T : new() {
+        public RawObject RawObject { get; }
+        public T Object { get; }
+
+        public RawTemplateObj() {
+            Object = new T();
+            RawObject = new RawObject();
+        }
+    }
+}
